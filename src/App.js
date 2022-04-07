08bf53131c9bd01d1ad2b75abfa9b19d1ddcc504
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-07 12:43:12 -0700

    grid squares can be clicked

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 function App() {
-  console.log("Hello");
-  return <div id="app">Hello</div>;
+  let rowsArr = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log(e.target.id, "Clicked!");
+  }
+
+  return (
+    <div id="app">
+      <div className="grid">
+        <div className="row" id="row1">
+          <div className="square" id="s1" onClick={(e) => handleClick(e)}>
+            {rowsArr[0][0]}
+          </div>
+          <div className="square" id="s2" onClick={(e) => handleClick(e)}>
+            {rowsArr[0][1]}
+          </div>
+          <div className="square" id="s3" onClick={(e) => handleClick(e)}>
+            {rowsArr[0][2]}
+          </div>
+        </div>
+
+        <div className="row" id="row2">
+          <div className="square" id="s4" onClick={(e) => handleClick(e)}>
+            {rowsArr[1][0]}
+          </div>
+          <div className="square" id="s5" onClick={(e) => handleClick(e)}>
+            {rowsArr[1][1]}
+          </div>
+          <div className="square" id="s6" onClick={(e) => handleClick(e)}>
+            {rowsArr[1][2]}
+          </div>
+        </div>
+
+        <div className="row" id="row3">
+          <div className="square" id="s7" onClick={(e) => handleClick(e)}>
+            {rowsArr[2][0]}
+          </div>
+          <div className="square" id="s8" onClick={(e) => handleClick(e)}>
+            {rowsArr[2][1]}
+          </div>
+          <div className="square" id="s9" onClick={(e) => handleClick(e)}>
+            {rowsArr[2][2]}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
-import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
+
 const root = createRoot(container);
+
 root.render(<App tab="home" />);
